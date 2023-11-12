31bbe2fa917989334e77b1c7bea118377ab0538a
Kamil Drzastwa <drzastwakamil@gmail.com>
2023-11-12 17:25:35 +0100

    [CONFIGURATION] Adding tailwind.css

--- app.vue
+++ app.vue
@@ -1,3 +1,5 @@
 <template>
-  <div />
+  <div class="bg-red-100">
+    Petcation
+  </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/eslint-module', '@nuxt/image']
+  modules: [
+    '@nuxtjs/eslint-module',
+    '@nuxt/image',
+    '@nuxtjs/tailwindcss'
+  ]
 })
