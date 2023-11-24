2f3105483e746bffcbf92f4e1a1ac40afa26a4ea
Kamil Drzastwa <drzastwakamil@gmail.com>
2023-11-24 11:48:52 +0100

    [Feature] Polishing the hotel card

--- components/hotelCard/HotelCard.vue
+++ components/hotelCard/HotelCard.vue
@@ -1,17 +1,48 @@
 <template>
   <NuxtLink to="/hotels/123">
-    <Card class="overflow-hidden">
-      <img class="h-60 w-full" src="/images/mock_hotel.webp" />
+    <Card class="relative overflow-hidden border-2 hover:border-black">
+      <Badge class="absolute left-3 top-3 z-10 px-3 py-2">
+        <StarIcon class="mr-2 h-4 w-4" />
+        {{ reviewAverage }}
+      </Badge>
+
+      <img class="h-60 w-full object-cover" :src="photoUrl" />
       <CardHeader>
-        <CardTitle>Card Title</CardTitle>
-        <CardDescription>Card Description</CardDescription>
+        <CardTitle>{{ title }}</CardTitle>
+        <CardDescription class="flex gap-1">
+          <div class="flex items-center">
+            <MapPinIcon class="mr-1 h-3.5 w-3.5" /> <span>{{ location }}</span>
+          </div>
+          <span> ~ {{ range }} km</span></CardDescription
+        >
       </CardHeader>
-      <CardContent> Card Content </CardContent>
-      <CardFooter> Card Footer </CardFooter>
+      <CardContent> {{ description }} </CardContent>
+      <CardFooter class="text-sm">
+        <span>
+          <span>
+            <span class="font-medium"> {{ pricePerNight }} zł</span> noc
+          </span>
+          -
+          <span class="font-thin"> {{ priceTotal }} całość </span>
+        </span>
+      </CardFooter>
     </Card>
   </NuxtLink>
 </template>
 
 <script setup lang="ts">
+import { StarIcon, MapPinIcon } from 'lucide-vue-next';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { Badge } from '@/components/ui/badge';
+defineProps<{
+  url: string;
+  photoUrl: string;
+  title: string;
+  location: string;
+  description: string;
+  reviewAverage: number;
+  range: number;
+  pricePerNight: number;
+  priceTotal: number;
+}>();
 </script>
--- pages/hotels/index.vue
+++ pages/hotels/index.vue
@@ -2,17 +2,18 @@
   <div class="container mx-auto bg-transparent">
     <div class="h-header" />
     <div class="flex justify-center gap-2 pt-10"><Combobox /> <DatePicker /> <AnimalsPicker /></div>
-    <div class="grid grid-cols-4 gap-4 pt-10">
-      <HotelCard />
-      <HotelCard />
-      <HotelCard />
-      <HotelCard />
-      <HotelCard />
-      <HotelCard />
-      <HotelCard />
-      <HotelCard />
-      <HotelCard />
-      <HotelCard />
+    <div class="grid grid-cols-4 gap-5 py-10">
+      <HotelCard
+        v-for="n in 30"
+        :key="n"
+        :location="'Głogowska 145, Poznań'"
+        :photo-url="`https://loremflickr.com/500/500/dog?idk=${Date.now() + Math.random()}`"
+        :price-per-night="150"
+        :price-total="7 * 150"
+        :range="8.5"
+        :review-average="4.56"
+        :title="'Hotel Psitulas'"
+      />
     </div>
   </div>
 </template>
